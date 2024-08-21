901c536b047a91c901231dae1369bff9aa6dd46d
abutahirhasan <athasan242@gmail.com>
2024-08-22 03:29:39 +0600

    adding-gsap

--- assets/js/main.js
+++ assets/js/main.js
@@ -478,7 +478,302 @@ CSS TABLE OF CONTENTS
 		$(".search-trigger, .main-search-input").on("click", function (e) {
 			e.stopPropagation();
 		});
-	}); // End Document Ready Function
+
+		//---------Use In Gsap--------
+		// lenis Scroll Init
+		// gsap.registerPlugin(ScrollSmoother);
+		// const lenis = new Lenis();
+		// gsap.ticker.add(function (time) {
+		//   lenis.raf(time * 400);
+		// });
+		// gsap.ticker.lagSmoothing(0);
+		// ScrollTrigger.update();
+
+		//--- Custom Tilt Js Start ---//
+		const tilt = document.querySelectorAll(".tilt");
+		VanillaTilt.init(tilt, {
+			reverse: true,
+			max: 15,
+			speed: 400,
+			scale: 1.01,
+			glare: true,
+			reset: true,
+			perspective: 800,
+			transition: true,
+			"max-glare": 0.45,
+			"glare-prerender": false,
+			gyroscope: true,
+			gyroscopeMinAngleX: -45,
+			gyroscopeMaxAngleX: 45,
+			gyroscopeMinAngleY: -45,
+			gyroscopeMaxAngleY: 45,
+		});
+		//--- Custom Tilt Js End ---//
+
+		// Mouse Follower
+		const follower = document.querySelector(
+			".mouse-follower .cursor-outline"
+		);
+		const dot = document.querySelector(".mouse-follower .cursor-dot");
+		window.addEventListener("mousemove", (e) => {
+			follower.animate(
+				[
+					{
+						opacity: 1,
+						left: `${e.clientX}px`,
+						top: `${e.clientY}px`,
+						easing: "ease-in-out",
+					},
+				],
+				{
+					duration: 3000,
+					fill: "forwards",
+				}
+			);
+			dot.animate(
+				[
+					{
+						opacity: 1,
+						left: `${e.clientX}px`,
+						top: `${e.clientY}px`,
+						easing: "ease-in-out",
+					},
+				],
+				{
+					duration: 1500,
+					fill: "forwards",
+				}
+			);
+		});
+
+		// Mouse Follower Hide Function
+		$("a, button").on("mouseenter mouseleave", function () {
+			$(".mouse-follower").toggleClass("hide-cursor");
+		});
+
+		var terElement = $(
+			"h1, h2, h3, h4, .display-one, .display-two, .display-three, .display-four, .display-five, .display-six"
+		);
+		$(terElement).on("mouseenter mouseleave", function () {
+			$(".mouse-follower").toggleClass("highlight-cursor-head");
+			$(this).toggleClass("highlight-cursor-head");
+		});
+
+		var terElement = $("p");
+		$(terElement).on("mouseenter mouseleave", function () {
+			$(".mouse-follower").toggleClass("highlight-cursor-para");
+			$(this).toggleClass("highlight-cursor-para");
+		});
+
+		//-- Use Gsap Animation --//
+		// Visible From Right Animation
+		const observer = new IntersectionObserver(
+			(entries, observer) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						let visibleFromRight = entry.target;
+						let split_item = new SplitText(visibleFromRight, {
+							type: "chars, words",
+						});
+						gsap.from(split_item.chars, {
+							duration: 0.4,
+							x: 45,
+							autoAlpha: 0,
+							stagger: 0.15,
+						});
+						observer.unobserve(visibleFromRight);
+					}
+				});
+			},
+			{ threshold: 0.1 }
+		);
+		document.querySelectorAll(".visible-from-right").forEach((element) => {
+			observer.observe(element);
+		});
+
+		// Visible From Right Slowly Animation
+		let visibleSlowlyRight = document.querySelectorAll(
+			".visible-slowly-right"
+		);
+		if ($(visibleSlowlyRight).length > 0) {
+			let char_come = gsap.utils.toArray(visibleSlowlyRight);
+			char_come.forEach((char_come) => {
+				let split_char = new SplitText(char_come, {
+					type: "chars, words",
+					lineThreshold: 0.5,
+				});
+				const tl2 = gsap.timeline({
+					scrollTrigger: {
+						trigger: char_come,
+						start: "top 90%",
+						end: "bottom 60%",
+						scrub: false,
+						markers: false,
+						toggleActions: "play none none none",
+					},
+				});
+				tl2.from(split_char.chars, {
+					duration: 0.8,
+					x: 70,
+					autoAlpha: 0,
+					stagger: 0.03,
+				});
+			});
+		}
+
+		// Visible From Bottom Animation
+		let visibleFromBottom = gsap.utils.toArray(".visible-from-bottom");
+		visibleFromBottom.forEach((splitArea) => {
+			const trigger = gsap.timeline({
+				scrollTrigger: {
+					trigger: splitArea,
+					start: "top 90%",
+					end: "bottom 60%",
+					scrub: false,
+					markers: false,
+				},
+			});
+			const contentSplitted = new SplitText(splitArea, {
+				type: "words, lines",
+			});
+			gsap.set(splitArea, { perspective: 400 });
+			contentSplitted.split({ type: "lines" });
+			trigger.from(contentSplitted.lines, {
+				duration: 1,
+				delay: 0.3,
+				opacity: 0,
+				rotationX: -75,
+				force3D: true,
+				transformOrigin: "top center -50",
+				stagger: 0.1,
+			});
+		});
+
+		// Visible Slowly From Bottom Animation
+		const visibleSlowlyBottom = document.querySelectorAll(
+			".visible-slowly-bottom"
+		);
+		function visibleSlowly() {
+			visibleSlowlyBottom.forEach((splitArea) => {
+				if (splitArea.anim) {
+					splitArea.anim.progress(1).kill();
+					splitArea.split.revert();
+				}
+				splitArea.split = new SplitText(splitArea, {
+					type: "lines,words,chars",
+					linesClass: "split-line",
+				});
+				splitArea.anim = gsap.from(splitArea.split.chars, {
+					scrollTrigger: {
+						trigger: splitArea,
+						toggleActions: "restart pause resume reverse",
+						start: "top 90%",
+					},
+					duration: 0.8,
+					ease: "circ.out",
+					y: 70,
+					stagger: 0.02,
+				});
+			});
+		}
+		ScrollTrigger.addEventListener("refresh", visibleSlowly);
+		visibleSlowly();
+
+		// Btn Custom Animation
+		// button Vivacity
+		let btnVivacity = document.querySelectorAll(".btn-vivacity");
+		if (btnVivacity) {
+			VanillaTilt.init(btnVivacity, {
+				max: 14,
+				speed: 2800,
+				perspective: 500,
+			});
+		}
+
+		// Box Style
+		const targetBtn = document.querySelectorAll(".box-style");
+		if (targetBtn) {
+			targetBtn.forEach((element) => {
+				element.addEventListener("mousemove", (e) => {
+					const x = e.offsetX + "px";
+					const y = e.offsetY + "px";
+					element.style.setProperty("--x", x);
+					element.style.setProperty("--y", y);
+				});
+			});
+		}
+
+		// image right to left
+		gsap.registerPlugin(ScrollTrigger);
+		let revealContainers = document.querySelectorAll(".reveal-one");
+		revealContainers.forEach((container) => {
+			let image = container.querySelector(".reveal-image-one");
+			let rts = gsap.timeline({
+				scrollTrigger: {
+					trigger: container,
+					toggleActions: "restart none none reset",
+					start: "top 90%",
+					end: "top 0%",
+				},
+			});
+			rts.set(container, {
+				autoAlpha: 1,
+			});
+			rts.from(container, 1.5, {
+				xPercent: 100,
+				ease: Power2.out,
+			});
+			rts.from(image, 1.5, {
+				xPercent: -100,
+				scale: 1.3,
+				delay: -1.5,
+				ease: Power2.out,
+			});
+		});
+
+		//Reveal SlideTop
+		function revealSlideTop() {
+			const reveals = document.querySelectorAll(".revealSlideTop");
+			const windowHeight = window.innerHeight;
+			const elementVisible = 150;
+
+			reveals.forEach((reveal) => {
+				const elementTop = reveal.getBoundingClientRect().top;
+
+				if (elementTop < windowHeight - elementVisible) {
+					reveal.classList.add("active");
+				} else {
+					reveal.classList.remove("active");
+				}
+			});
+		}
+		window.addEventListener("scroll", revealSlideTop);
+
+		// reveal-fourth
+		let revealContainersFourth = document.querySelectorAll(".reveal-fourth");
+		revealContainersFourth.forEach((container) => {
+			let image = container.querySelector("img");
+			let tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: container,
+					toggleActions: "restart none none reset",
+				},
+			});
+			tl.set(container, { autoAlpha: 1 });
+			tl.from(container, 1.5, {
+				xPercent: 100,
+				ease: Power2.out,
+			});
+			tl.from(image, 1.5, {
+				xPercent: -100,
+				scale: 1.3,
+				delay: -1.5,
+				ease: Power2.out,
+			});
+		});
+
+		// End Document Ready Function
+	});
 
 	function loader() {
 		$(window).on("load", function () {
